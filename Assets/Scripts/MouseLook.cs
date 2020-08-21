680f3663d06d3c2be25056047e9e71da6dc14240
EpsilonTheDerg <jamesclamey@gmail.com>
2020-08-21 17:09:22 -0400

    no message

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 125f;
     public Transform playerBody;
     float xRotation = 0f;
     
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
-    public float gravity = -9.81f;
+    public float gravity = -19.62f;
     public float jumpHeight = 3f;
 
     public Transform groundCheck;
