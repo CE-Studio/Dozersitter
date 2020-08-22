dc1d2bfd70e3c3bde1a5a88b23f54055155665ff
EpsilonTheDerg <jamesclamey@gmail.com>
2020-08-21 20:12:49 -0400

    no message

--- Assets/Scripts/DozerAI.cs
+++ Assets/Scripts/DozerAI.cs
@@ -17,6 +17,8 @@ public class DozerAI : MonoBehaviour
     int waitWeight;
     int turnWeight;
     int moveWeight;
+    float speed = 10f;
+    Rigidbody rb;
     
     void Start()
     {
@@ -25,33 +27,69 @@ public class DozerAI : MonoBehaviour
         waitWeight = Random.Range(25, 40);
         turnWeight = Random.Range(20, 30);
         moveWeight = Random.Range(20, 30);
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (dozerState == "Idle")
+        switch (dozerState)
         {
-            randomInt = Random.Range(1, 100);
-            if (randomInt <= waitWeight)
-            {
-                dozerState = "Wait";
-                randomInt = Random.Range(10, 30);
-            }
-            else if (randomInt > waitWeight && randomInt <= waitWeight + turnWeight)
-            {
-                dozerState = "Turn";
-                randomInt = Random.Range(-180, 180);
-            }
-            else if (randomInt > waitWeight + turnWeight && randomInt <= waitWeight + turnWeight + moveWeight)
-            {
-                dozerState = "Move";
-                randomInt = Random.Range(10, 40);
-            }
-            else
-            {
-                dozerState = "Inspect";
-            }
+            case "Idle":
+                randomInt = Random.Range(1, 100);
+                if (randomInt <= waitWeight)
+                {
+                    dozerState = "Wait";
+                    randomInt = Random.Range(10, 30);
+                }
+                else if (randomInt > waitWeight && randomInt <= waitWeight + turnWeight)
+                {
+                    dozerState = "Turn";
+                    randomInt = Random.Range(-180, 180);
+                }
+                else if (randomInt > waitWeight + turnWeight && randomInt <= waitWeight + turnWeight + moveWeight)
+                {
+                    dozerState = "Move";
+                    randomInt = Random.Range(10, 40);
+                }
+                else
+                {
+                    dozerState = "Inspect";
+                }
+                break;
+            case "Wait":
+                randomInt--;
+                if (randomInt >= 0)
+                {
+                    dozerState = "Idle";
+                }
+                break;
+            case "Turn":
+                if (transform.rotation.y > randomInt)
+                {
+                    transform.Rotate(Vector3.up, -speed * Time.deltaTime);
+                }
+                else if (transform.rotation.y < randomInt)
+                {
+                    transform.Rotate(Vector3.up, speed * Time.deltaTime);
+                }
+                else if (transform.rotation.y >= randomInt + 3 && transform.rotation.y <= randomInt - 3)
+                {
+                    dozerState = "Idle";
+                }
+                break;
+            case "Move":
+                Vector3 movement = transform.forward * speed * Time.deltaTime;
+                rb.MovePosition(movement);
+                randomInt--;
+                if (randomInt >= 0)
+                {
+                    dozerState = "Idle";
+                }
+                break;
+            case "Inspect":
+                dozerState = "Idle";
+                break;
         }
-        print("The dozer had a " + waitWeight + "% chance to wait, a " + turnWeight + "% chance to turn, and a " + moveWeight + "% chance to move. It chose to " + dozerState);
+        //print("The dozer had a " + waitWeight + "% chance to wait, a " + turnWeight + "% chance to turn, and a " + moveWeight + "% chance to move. It chose to " + dozerState);
     }
 }
