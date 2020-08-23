d873ac641b4c65ab35d1977fe7d13dd81d6a0c68
EpsilonTheDerg <jamesclamey@gmail.com>
2020-08-23 00:12:43 -0400

    no message

---
+++ Assets/Scripts/MainMenu/GlobalVarTracker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVarTracker : MonoBehaviour
+{
+    public static int mouseSensitivity = 250;
+    public static int volume = 50;
+}
