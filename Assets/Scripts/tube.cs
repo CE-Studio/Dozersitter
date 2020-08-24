dade2d1f00c4164e6660bc2b2619bc7841d6a4a0
clarence112 <clarencepacelli@gmail.com>
2020-08-24 01:26:40 -0400

    no message

---
+++ Assets/Scripts/tube.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tube : MonoBehaviour {
+    void OnCollisionEnter(Collision collision) {
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 100, -200));
+    }
+}
