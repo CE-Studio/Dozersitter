30977e85bb57d470006a979e9f2efaebd162e965
EpsilonTheDerg <jamesclamey@gmail.com>
2020-08-24 11:03:46 -0400

    no message

---
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+}
